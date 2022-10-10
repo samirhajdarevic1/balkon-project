14a17e9e233a2482579fc7fb217384e860571cf9
Samir <samirhajdarevic1995@gmail.com>
2022-10-10 19:32:54 +0200

    Added forms for two components

---
+++ client/src/components/EditPredmetForm.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { ucitajPredmet, urediPredmet } from '../redux/predmeti/actions';
+import formStyles from './Form.module.css';
+
+const EditPredmetForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { idPredmet } = useParams();
+  const [predmet] = useSelector((state) =>
+    state.predmeti.items.filter((pred) => pred.idPredmet === +idPredmet)
+  );
+  const tmpNaziv = (predmet && predmet.naziv) || '';
+  const [naziv, setNaziv] = useState(tmpNaziv);
+
+  useEffect(() => {
+    if (!predmet) {
+      dispatch(ucitajPredmet(+idPredmet));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (predmet) setNaziv(predmet.naziv);
+  }, [predmet]);
+  const editingHandler = (e) => {
+    e.preventDefault();
+    dispatch(urediPredmet({ idPredmet, naziv })).then(() =>
+      navigate(`/predmeti/${idPredmet}`)
+    );
+  };
+
+  return (
+    <form onSubmit={editingHandler} className={formStyles['form-control']}>
+      <div>
+        <ul>
+          <li>
+            <input
+              value={naziv}
+              onChange={(e) => setNaziv(e.target.value)}
+              type="text"
+            />
+          </li>
+        </ul>
+      </div>
+      <button>Done editing</button>
+    </form>
+  );
+};
+
+export default EditPredmetForm;
